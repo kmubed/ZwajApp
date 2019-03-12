266c20be57d8ea6c4620540da75c0111738dea8a
kmubed <mubed_77@yahoo.com>
2019-03-12 12:15:18 +0300

    End of seation 7

--- ZwajApp-SPA/src/app/app.module.ts
+++ ZwajApp-SPA/src/app/app.module.ts
@@ -17,6 +17,7 @@ import { MemberListComponent } from './member-list/member-list.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
 import { appRoutes } from './routes';
+import { AuthGuard } from './_guards/auth.guard';
 
 @NgModule({
    declarations: [
@@ -34,11 +35,13 @@ import { appRoutes } from './routes';
       FormsModule,
       BsDropdownModule.forRoot(),
       RouterModule.forRoot(appRoutes)
+      
    ],
    providers: [
       AuthService,
       ErrorInterceptorProvidor,
-      AlertifyService
+      AlertifyService,
+      AuthGuard
    ],
    bootstrap: [
       AppComponent
